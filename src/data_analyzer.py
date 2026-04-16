6f3e4ae231167e6288a72752960d8af8be45c03b
Rebecca120805 <rep6136@thi.de>
2026-04-16 14:11:08 +0200

    Ergänzen docstrings

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -1,11 +1,24 @@
 import pandas as pd
 
 def get_daily_total(df):
-  """Gibt die gesamte Bildschirmzeit pro Tag zurück"""
+  """Gibt die gesamte Bildschirmzeit pro Tag zurück.
+
+    Die Funktion gruppiert die übergebenen Screentime-Daten nach Datum und summiert die Nutzungsdauer aller Apps für jeden einzelnen Tag.
+
+    :param df: Pandas DataFrame mit den Spalten
+      - 'date' (Datum in Format YYYY-MM-DD)
+      - 'usage_minutes' (Nutzungsdauer in Minuten)
+
+    :return: Pandas Series mit dem Datum als INdex und der gesamten Nutzungszeit pro tag als Wert"""
   return df.groupby("date")["usage_minutes"].sum()
 
+
 def get_top_5_apps_per_day(df):
-  """Gibt für jeden Tag die Top 5 meistgenutzten Apps zurück."""
+  """Gibt für jeden Tag die Top 5 meistgenutzten Apps zurück.
+
+    :param df: Pandas DataFrame mit ScreenTime-Daten
+        (Spalten: 'date', 'app', 'usage_minutes')
+    :return: Dictionary mit Datum als Key und einer Pandas Series (Top-5-Apps pro Tag) als Value"""
   result = {}
   for date, group in df.groupby("date"): #Daten werden nach Tagen getrennt
     top5 = (
@@ -18,8 +31,10 @@ def get_top_5_apps_per_day(df):
 
   return result
 
+
 def get_top_5_total_time(top5_dict):
-  """Berechnet für jeden Tag die Summe der Nutzungszeit der jeweiligen Top 5 Apps
+  """Berechnet für jeden Tag die Summe der Nutzungszeit der jeweiligen Top 5 Apps.
+
       :param top5_dict: Dictionary mit Datum als Key und Pandas Series (Top-5-Apps) als Value
       :return: Dictionary mit Datum und Gesamtzeit der Top-5-Apps"""
   totals= {}
