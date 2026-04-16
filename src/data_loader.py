e0ac8cc817ccd086d2a8a277a93b000e2ed65d09
Rebecca120805 <rep6136@thi.de>
2026-04-16 13:46:25 +0200

    Schreibfehler ausgebessert

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data (path="data/screentime.csv"):
-  """Lädt die Screentime Daten aus der CSV-Datei und gibt ein DataFrame zurück.
+  """Lädt die screentime Daten aus der CSV-Datei und gibt ein DataFrame zurück.
       :param: path: Pfad zur CSV-Datei
       :return: Pandas DataFrame mit Screentime-Daten"""
   return pd.read_csv(path)
